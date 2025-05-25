bd63d71dc7f8ec69db8c46c732f56751c1d78dd7
hmiwx <hamidrezamozafarinasab@gmail.com>
2025-05-25 23:25:48 +0330

    add font and basic styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+//import './assets/styles/reset.css';
+import './assets/styles/fonts.css';
+import './assets/styles/variables.css';
+//import './assets/styles/helpers.css';
+//import './assets/styles/defaults.css';
 import './index.css'
 import App from './App.jsx'
 
