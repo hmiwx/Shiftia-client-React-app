fb81bd01c70ee88d2f1ba4479de0242119f46061
hmiwx <hamidrezamozafarinasab@gmail.com>
2025-05-26 22:23:26 +0330

    add header jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <section className="text-center mb-16 fade-in">
+            <h2 className="text-5xl font-bold text-white mb-4">پلتفرم رزرو خدمات محلی برای کارکنان شیفتی</h2>
+            <p className="text-gray-200 mt-4 text-xl max-w-2xl mx-auto">تعمیرات سریع، رزرو کارواش و آرایشگاه، نظافت محل و...</p>
+            <div className="mt-8 flex gap-4 justify-center">
+                <Link to="#mvp-video" className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-3 rounded-full pulse transition-all shadow-lg">
+                    مشاهده ویدیو معرفی
+                </Link>
+                <Link to="/" className="bg-white text-blue-600 hover:bg-blue-50 px-8 py-3 rounded-full transition-all shadow-lg">
+                    ثبت‌نام رایگان
+                </Link>
+            </div>
+        </section>
+  )
+}
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Topbar from '../../components/Topbar/Topbar'
+import Header from '../../components/Header/Header'
 
 export default function Landing() {
   return (
     <>
     <Topbar />
-    
+    <main class="max-w-6xl mx-auto mt-8 p-4">
+      <Header />
+    </main>
     </>
   )
 }
