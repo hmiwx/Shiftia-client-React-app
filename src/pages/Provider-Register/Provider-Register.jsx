1d154785eaa6cb3a2baaccf121292c82ad652ea9
hmiwx <hamidrezamozafarinasab@gmail.com>
2025-05-30 20:01:26 +0330

    handle validation styles

--- src/pages/Provider-Register/Provider-Register.jsx
+++ src/pages/Provider-Register/Provider-Register.jsx
@@ -97,7 +97,7 @@ export default function Provider_Register() {
                             <Input
                                 type="text"
                                 placeholder="علیرضا"
-                                className="w-full p-3 pl-10"
+                                className={`w-full p-3 pl-10 ${formState.inputs.name.isValid ? "border-green-500" : "border-red-500"}`}
                                 element="input"
                                 id="name"
                                 iconClasses="fas fa-user input-icon absolute inset-y-4 left-2 flex items-center pl-3 text-gray-400 pointer-events-none"
@@ -116,7 +116,7 @@ export default function Provider_Register() {
                             <Input
                                 type="text"
                                 placeholder="09121111111"
-                                className="w-full p-3 pl-10"
+                                className={`w-full p-3 pl-10 ${formState.inputs.phone.isValid ? "border-green-500" : "border-red-500"}`}
                                 element="input"
                                 id="phone"
                                 iconClasses="fas fa-phone input-icon absolute inset-y-4 left-2 flex items-center pl-3 text-gray-400 pointer-events-none"
@@ -134,7 +134,7 @@ export default function Provider_Register() {
                             <Input
                                 type="email"
                                 placeholder="info@gmail.com"
-                                className="w-full p-3 pl-10"
+                                className={`w-full p-3 pl-10 ${formState.inputs.email.isValid ? "border-green-500" : "border-red-500"}`}
                                 element="input"
                                 id="email"
                                 iconClasses="fas fa-envelope absolute inset-y-4 left-2 flex items-center pl-3 text-gray-400 pointer-events-none"
@@ -150,7 +150,7 @@ export default function Provider_Register() {
                         <label htmlFor="service" class="form-label">نوع خدمت</label>
                         <div class="relative">
                             <Input
-                                className="w-full p-3 pl-10"
+                                className={`w-full p-3 pl-10 ${formState.inputs.service.isValid ? "border-green-500" : "border-red-500"}`}
                                 element="select"
                                 id="service"
                                 name="service"
@@ -180,7 +180,7 @@ export default function Provider_Register() {
                             <Input
                                 type="text"
                                 placeholder="عنوان مشاغل"
-                                className="w-full p-3 pl-10"
+                                className={`w-full p-3 pl-10 ${formState.inputs.reqjob.isValid ? "border-green-500" : "border-red-500"}`}
                                 element="input"
                                 id="reqjob"
                                 iconClasses="fas fa-briefcase absolute inset-y-4 left-2 flex items-center pl-3 text-gray-400 pointer-events-none"
